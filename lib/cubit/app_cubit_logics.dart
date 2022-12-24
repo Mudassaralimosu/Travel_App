3ae1f85f844eb56d18d480b209a6ab03b06dbb3f
mudassaralimosu <mudassar786mosu@gmail.com>
2022-12-25 02:22:48 +0530

    Flutter Travel App

---
+++ lib/cubit/app_cubit_logics.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit_states.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
+import 'package:travel_app/pages/welcome_page.dart';
+
+import '../pages/detail_page.dart';
+import '../pages/home_page.dart';
+import 'app_cubits.dart';
+
+class AppCubitLogics extends StatefulWidget {
+  AppCubitLogics({Key? key}) : super(key: key);
+
+  @override
+  State<AppCubitLogics> createState() => _AppCubitLogicsState();
+}
+
+class _AppCubitLogicsState extends State<AppCubitLogics> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<AppCubits, CubitStates>(
+        builder: (context, state) {
+          if (state is DetailState) {
+            return DetailPage();
+          }
+          if (state is WelcomeState) {
+            return WelcomePage();
+          }
+          if (state is LoadedState) {
+            return MainPage();
+            //return HomePage();
+          }
+          if (state is LoadingState) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit_logics.dart';
+import 'package:travel_app/cubit/app_cubits.dart';
+import 'package:travel_app/pages/detail_page.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
+import 'package:travel_app/pages/welcome_page.dart';
+import 'package:travel_app/services/data_services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      //home: WelcomePage()
+      //home: MainPage()
+      //home:DetailPage()
+
+      home: BlocProvider<AppCubits>(
+          create: (context)=>AppCubits(
+            data: DataServices(),
+          ),
+          child: AppCubitLogics(),
+      )
+
+    );
+  }
+}
